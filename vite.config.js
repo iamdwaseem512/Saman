996f1932a2a34b31053c39fa7cc73d2ebf70735c
iamdwaseem <iamdwaseem@gmail.com>
2025-10-07 00:17:57 +0530

    login page error fixed

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
         contact: resolve(__dirname, "contact.html"),
         products: resolve(__dirname, "products.html"),
         addToCart: resolve(__dirname, "addToCart.html"),
+        signin: resolve(__dirname, "signin.html"),
+        signup: resolve(__dirname, "signup.html"),
+
         // Add more entry points for other HTML files as needed
       },
     },
